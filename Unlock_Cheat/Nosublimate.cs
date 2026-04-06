9ce717cd9b56fa2f5f66ca453488a48e05da1baa
Crazy-xyr <979817246@qq.com>
2026-04-06 22:06:43 +0800

    更新游戏719533版本

--- Unlock_Cheat/Nosublimate.cs
+++ Unlock_Cheat/Nosublimate.cs
@@ -1,15 +1,23 @@
 using HarmonyLib;
-
+using System.Collections.Generic;
 
 namespace Unlock_Cheat.Nosublimate
 {
+
+
     [HarmonyPatch(typeof(Sublimates), "Sim200ms")]
-    public static class Sublimates_Sim200ms
+    public class Sublimates_Sim200ms
     {
+        private static List<SimHashes> Sublimates_Elements = null;
+
         public static bool Prefix(Sublimates __instance, float dt)
         {
+            if (Sublimates_Elements == null) {
+
+                Sublimates_Elements =  Unlock_Cheat.Options.GetSublimates();
+            }
             SimHashes elementID = __instance.GetComponent<PrimaryElement>().ElementID;
-            return elementID != SimHashes.SlimeMold && elementID != SimHashes.ToxicSand && elementID != SimHashes.ToxicMud && elementID != SimHashes.BleachStone && elementID != SimHashes.DirtyWater;
+            return !Sublimates_Elements.Contains(elementID);
         }
     }
 }
